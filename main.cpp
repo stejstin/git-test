9172080e0a0acc747ac9ec47fc1204cc09929d24
Stejstin <stejstin@ut.utm.edu>
2016-08-30 17:51:04 -0500

    new main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int sum(int n){
+  int sum = 0;
+  for(int i = 1; i <= n; i++){
+     sum += i;
+  }
+  return sum;
+}			     
+	
 
 int main(){
 
   cout << "Hello world" << endl;
+  cout << "please enter a number" << endl;
+  int n;
+  cin >> n;
+  cout << sum(n);
 
 }
